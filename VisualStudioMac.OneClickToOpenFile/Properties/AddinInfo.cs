8f15b3b2f60b5a22c9fc3af1295f76250bde1b6c
Ivo Krugers <i.krugers@xablu.com>
2022-06-08 08:18:31 +0200

    Adjust addin info

--- VisualStudioMac.OneClickToOpenFile/Properties/AddinInfo.cs
+++ VisualStudioMac.OneClickToOpenFile/Properties/AddinInfo.cs
@@ -9,7 +9,7 @@ using VisualStudioMac.OneClickToOpenFile;
     Category = "IDE extensions"
 )]
 
-[assembly: AddinName("SolutionTree One Click To Open File")]
+[assembly: AddinName("One Click To Open File")]
 [assembly: AddinDescription("This extension opens a file with only one click on the solutiontree.\n\nby Ivo Krugers")]
 [assembly: AddinAuthor("Ivo Krugers")]
 [assembly: AddinUrl("https://github.com/IvoKrugers/VisualStudioMac.OneClickToOpenFile")]
