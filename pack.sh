723a622adad26d364a7ae7fe314339fe20b7e745
Ivo Krugers <ivo.krugers@gmail.com>
2022-11-29 18:36:01 +0100

    fix script

--- pack.sh
+++ pack.sh
@@ -15,17 +15,17 @@ rm *.mpack
 
 # Pack
 #mono /Applications/Visual\ Studio\ \(2019\).app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ./VisualStudioMac.OneClickToOpenFile/bin/VisualStudioMac.OneClickToOpenFile.dll
-/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool setup pack /Users/ivokrugers/Xamarin_Projects/VisualStudioMac.OneClickToOpenFile/VisualStudioMac.OneClickToOpenFile/bin/VisualStudioMac.OneClickToOpenFile.dll
+/Applications/Visual\ Studio.app/Contents/MacOS/vstool setup pack /Users/ivokrugers/Xamarin_Projects/VisualStudioMac.OneClickToOpenFile/VisualStudioMac.OneClickToOpenFile/bin/VisualStudioMac.OneClickToOpenFile.dll
 
 # Copy to local dir
-for filename in /Applications/Visual\ Studio\ \(Preview\).app/*OneClickToOpenFile*.mpack;
+for filename in /Applications/Visual\ Studio.app/*OneClickToOpenFile*.mpack;
 do
   echo "move $filename"
   mv "$filename" .
 done
 
 # Uninstall
-/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool setup uninstall VisualStudioMac.OneClickToOpenFile -y
+/Applications/Visual\ Studio.app/Contents/MacOS/vstool setup uninstall VisualStudioMac.OneClickToOpenFile -y
 
 # # Install
 # for filename in *.mpack;
