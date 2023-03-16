c1af3c0f0ec533bb83bafb47c188b576d423e533
Ivo Krugers <ivo.krugers@gmail.com>
2023-03-16 21:17:39 +0100

    bup

--- VisualStudioMac.OneClickToOpenFile/CommandHandlers/Node/OneClickNodeCommandHandler.cs
+++ VisualStudioMac.OneClickToOpenFile/CommandHandlers/Node/OneClickNodeCommandHandler.cs
@@ -11,57 +11,99 @@ namespace VisualStudioMac.OneClickToOpenFile.CommandHandlers.Node
 {
     public class OneClickNodeCommandHandler : NodeCommandHandler
     {
+        public static string LastFileOpened = "";
+
+        private static DateTime _lastOnItemSelected = DateTime.Now.AddMinutes(-5);
+
         public override void OnItemSelected()
         {
             base.OnItemSelected();
 
-            var selectedNode = Controller.GetSelectedNode();
-            var control = Controller.Control;
+            LogInfo("OnItemSelected");
 
             if (CurrentNode.DataItem is ProjectFile f && Settings.OneClickToOpenFileEnabled)
             {
-                string ext = Path.GetExtension(f.FilePath);
-                if (Constants.ExcludedExtensionsFromOneClick.FindIndex((s) => s == ext) == -1)
+                try
                 {
-                    var selectedItem = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
-
-                    if (IdeApp.IsRunning
-                        && IdeApp.IsInitialized
-                        && IdeApp.Workspace.CurrentSelectedSolution != null
-                        && IdeApp.Workbench.CurrentLayout != null
-                        && (IdeApp.Workbench.ActiveDocument == null || IdeApp.Workbench.ActiveDocument.FilePath.FullPath != f.FilePath.FullPath))
+                    if ((DateTime.Now - _lastOnItemSelected).TotalMilliseconds <= 200.0)
                     {
-                        Debug.WriteLine($"[OnItemSelected]: CurrentNode: {f.FilePath.FileName} - Selected: {CurrentNode.Selected}, CurrentSelectedItem: {selectedItem?.FilePath.FileName ?? ""}, ActiveDocument: {IdeApp.Workbench.ActiveDocument?.Name ?? ""}");
-
-
-                        //if (Controller.Control.HasFocus)
-                        //{
-                        IdeApp.Workbench.OpenDocument(f.FilePath, f.Project);
-                        //IdeApp.Workbench.OpenDocument(f.FilePath, f.Project, MonoDevelop.Ide.Gui.OpenDocumentOptions.OpenIfMissing | MonoDevelop.Ide.Gui.OpenDocumentOptions.BringToFront);
-                        //IdeApp.Workbench.OpenDocument(f.FilePath, f.Project, MonoDevelop.Ide.Gui.OpenDocumentOptions.TryToReuseViewer | MonoDevelop.Ide.Gui.OpenDocumentOptions.OpenIfMissing);
-                        //}
                         return;
                     }
+
+                    string ext = Path.GetExtension(f.FilePath);
+                    if (Constants.ExcludedExtensionsFromOneClick.FindIndex((s) => s == ext) == -1)
+                    {
+                        var selectedItem = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
+                        var selectedNode = Controller.GetSelectedNode();
+                        var control = Controller.Control;
+
+                        var fullFilename = GetFullFilename(f.FilePath);
+
+                        if (IdeApp.IsRunning
+                            && IdeApp.IsInitialized
+                            && IdeApp.Workspace.CurrentSelectedSolution != null
+                            //&& IdeApp.Workbench.CurrentLayout != null
+                            //&& (IdeApp.Workbench.ActiveDocument == null || IdeApp.Workbench.ActiveDocument.FilePath.FullPath != f.FilePath.FullPath)
+                            && string.Compare(LastFileOpened, fullFilename, StringComparison.Ordinal) != 0
+                            )
+                        {
+                            IdeApp.Workbench.OpenDocument(f.FilePath, f.Project);
+                            //IdeApp.Workbench.OpenDocument(f.FilePath, f.Project, MonoDevelop.Ide.Gui.OpenDocumentOptions.OpenIfMissing | MonoDevelop.Ide.Gui.OpenDocumentOptions.BringToFront);
+                            //IdeApp.Workbench.OpenDocument(f.FilePath, f.Project, MonoDevelop.Ide.Gui.OpenDocumentOptions.TryToReuseViewer | MonoDevelop.Ide.Gui.OpenDocumentOptions.OpenIfMissing);
+                        }
+
+                        LastFileOpened = fullFilename;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    _lastOnItemSelected = DateTime.Now;
                 }
             }
+        }
+
+        private string GetFullFilename(FilePath fp)
+        {
+            if (fp == null)
+                return "";
+
+            return GetFullFilename(fp.ParentDirectory) + "/" + fp.FileName;
         }
 
         //public override void RefreshItem()
         //{
         //    base.RefreshItem();
-        //    var selectedNode = Controller.GetSelectedNode();
-        //    var control = Controller.Control;
-        //    var f = CurrentNode.DataItem as ProjectFile;
-        //    var selectedItem = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
-
-        //    Debug.WriteLine($"[RefreshItem]: CurrentNode: {f.FilePath.FileName} - Selected: {CurrentNode.Selected}, CurrentSelectedItem: {selectedItem?.FilePath.FileName ?? ""}, ActiveDocument: {IdeApp.Workbench.ActiveDocument?.Name ?? ""}");
+        //    LogInfo("RefreshItem");
         //}
 
         //public override void ActivateItem()
         //{
         //    base.ActivateItem();
+        //    LogInfo("ActivateItem");
         //}
 
+        private void LogInfo(string methodname)
+        {
+            var selectedNode = Controller.GetSelectedNode();
+            var control = Controller.Control;
+            var f = CurrentNode.DataItem as ProjectFile;
+            var selectedItem = IdeApp.ProjectOperations.CurrentSelectedItem as ProjectFile;
 
+            var sb = new System.Text.StringBuilder();
+            sb.Append($"[{methodname}]:");
+            sb.Append($" CurrentNode.Filled: {CurrentNode.Filled.ToString().PadLeft(6)},");
+            sb.Append($" CurrentNode.DataItem.Filename: {f.FilePath.FileName.PadLeft(25)},");
+            sb.Append($" Selected: {CurrentNode.Selected.ToString().PadLeft(5)},");
+            sb.Append($" CurrentSelectedItem: {(selectedItem?.FilePath.FileName ?? "").PadLeft(25)},");
+            sb.Append($" ActiveDocument: {(IdeApp.Workbench.ActiveDocument?.Name ?? "").PadLeft(25)},");
+            sb.Append($" Workbench.HasToplevelFocus: {IdeApp.Workbench.HasToplevelFocus.ToString().PadLeft(5)},");
+            sb.Append($" Controller.Control.HasFocus: {control.HasFocus.ToString().PadLeft(5)}");
+
+            Debug.WriteLine(sb.ToString());
+        }
     }
 }
--- VisualStudioMac.OneClickToOpenFile/CommandHandlers/StartupCommandHandler.cs
+++ VisualStudioMac.OneClickToOpenFile/CommandHandlers/StartupCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Ide;
+using VisualStudioMac.OneClickToOpenFile.CommandHandlers.Node;
 
 namespace VisualStudioMac.OneClickToOpenFile.CommandHandlers
 {
@@ -9,34 +10,37 @@ namespace VisualStudioMac.OneClickToOpenFile.CommandHandlers
         protected override void Run()
         {
             Settings.OneClickToOpenFileEnabled = false;
-            IdeApp.Workspace.SolutionLoaded += Workspace_SolutionLoaded;
-            IdeApp.Workspace.SolutionUnloaded += (s, e) => Settings.OneClickToOpenFileEnabled = false;
+            Settings.OneClickToOpenFileEnabled = true;
+            //IdeApp.Workspace.SolutionLoaded += Workspace_SolutionLoaded;
+            //IdeApp.Workspace.SolutionUnloaded += (s, e) => Settings.OneClickToOpenFileEnabled = false;
 
-            IdeApp.Workspace.FileAddedToProject += (s, e) => OnActivateOneClickAfterDelay();
-            IdeApp.Workspace.FileRemovedFromProject += (s, e) => OnActivateOneClickAfterDelay();
-            IdeApp.Workspace.FileRenamedInProject += (s, e) => OnActivateOneClickAfterDelay();
+            //IdeApp.Workspace.FileAddedToProject += (s, e) => OnActivateOneClickAfterDelay();
+            //IdeApp.Workspace.FileRemovedFromProject += (s, e) => OnActivateOneClickAfterDelay();
+            //IdeApp.Workspace.FileRenamedInProject += (s, e) => OnActivateOneClickAfterDelay();
 
             //IdeApp.Workbench.LayoutChanged += Workbench_LayoutChanged;
             //IdeApp.Exiting += (s,e) => Settings.OneClickToOpenFileEnabled = false;
             //IdeApp.Workbench.GuiLocked += (s, e) => Settings.OneClickToOpenFileEnabled = false;
             //IdeApp.Workbench.GuiUnlocked += (s, e) => Settings.OneClickToOpenFileEnabled = true;
+
+            IdeApp.Workbench.DocumentClosed += (s, e) => OneClickNodeCommandHandler.LastFileOpened = string.Empty;
         }
 
         private void Workspace_SolutionLoaded(object sender, MonoDevelop.Projects.SolutionEventArgs e)
         {
-            if (e.Solution != null)
-            {
-                ActivateOneClickAfterDelay();
-            }
+            //if (e.Solution != null)
+            //{
+            //    ActivateOneClickAfterDelay();
+            //}
         }
 
         private void OnActivateOneClickAfterDelay()
         {
-            if (Settings.OneClickToOpenFileEnabled)
-            {
-                Settings.OneClickToOpenFileEnabled = false;
-                ActivateOneClickAfterDelay();
-            }
+            //if (Settings.OneClickToOpenFileEnabled)
+            //{
+            //    Settings.OneClickToOpenFileEnabled = false;
+            //    ActivateOneClickAfterDelay();
+            //}
         }
 
         private void ActivateOneClickAfterDelay()
